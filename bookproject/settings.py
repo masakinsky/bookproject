5ced980453ffc28a9c455c76a0dd53f13f4a33f2
unknown <usermasa2000@gmail.com>
2025-04-21 12:37:12 +0900

    'four'

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ol$f%f(lt8$emopg6d1h-2ep4$e6uets01vm2wa$(8afu+q6m4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+'django-insecure-ol$f%f(lt8$emopg6d1h-2ep4$e6uets01vm2wa$(8afu+q6m4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
